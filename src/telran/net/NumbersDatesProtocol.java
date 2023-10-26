eb68e44eb356724e7b9f797f3bb1ffbaf48eb63d
Alexander <caxagsh@gmail.com>
2023-10-26 20:37:39 +0300

    hw_39

--- src/telran/net/NumbersDatesProtocol.java
+++ src/telran/net/NumbersDatesProtocol.java
@@ -7,13 +7,13 @@ public class NumbersDatesProtocol implements ApplProtocol {
 
 	@Override
 	public  Response getResponse(Request request) {
-		String[]token = ((String) request.requestData()).split("#");
+		String[]token = (String[]) request.requestData();
 		 
 		String responseData = switch(request.requestType()) {
-		case "add" -> Double.toString(Double.parseDouble(token[1]) + Double.parseDouble(token[2]));
-		case "subtruct" -> Double.toString(Double.parseDouble(token[1]) - Double.parseDouble(token[2]));
-		case "divide" -> Double.toString(Double.parseDouble(token[1]) / Double.parseDouble(token[2]));
-		case "multiplay" -> Double.toString(Double.parseDouble(token[1]) * Double.parseDouble(token[2]));
+		case "add" -> Double.toString(Double.parseDouble(token[0]) + Double.parseDouble(token[1]));
+		case "subtruct" -> Double.toString(Double.parseDouble(token[0]) - Double.parseDouble(token[1]));
+		case "divide" -> Double.toString(Double.parseDouble(token[0]) / Double.parseDouble(token[1]));
+		case "multiplay" -> Double.toString(Double.parseDouble(token[0]) * Double.parseDouble(token[1]));
 		case "afterdays" -> LocalDate.parse(token[0]).plusDays(Long.parseLong(token[1])).toString();
 		case "bifordays" -> LocalDate.parse(token[0]).minusDays(Long.parseLong(token[1])).toString();
 		case "betweentodate" -> Long.toString(ChronoUnit.DAYS.between(LocalDate.parse(token[1]), LocalDate.parse(token[2]))); 
